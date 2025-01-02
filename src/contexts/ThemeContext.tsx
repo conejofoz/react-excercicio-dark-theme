cd483666ce436d513c31cbbea145fba891972ea3
conejofoz <conejofoz@gmail.com>
2025-01-02 12:26:00 -0300

    criando o contexto

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+const STORAGE_KEY = 'themeContextKey';
+
+type ThemeContextType = {
+    theme: string;
+    setTheme: (newTheme: string) => void;
+}
+export const ThemeContext = createContext<ThemeContextType | null >(null);
+
+
+
+export const ThemeProvider = ({children}: {children: ReactNode})=>{
+    const [theme, setTheme] = useState(
+        localStorage.getItem(STORAGE_KEY) || 'light'
+    );
+
+    useEffect(()=> {
+        localStorage.setItem(STORAGE_KEY, theme);
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value={ {theme, setTheme } } >
+        {children}
+        </ThemeContext.Provider>
+    )
+}
+
+
+export const useTheme = () => useContext(ThemeContext);
