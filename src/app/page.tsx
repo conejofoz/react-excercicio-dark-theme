1654b2c95c74d8ddf15f24e556e6d5436292a188
conejofoz <conejofoz@gmail.com>
2025-01-02 13:09:00 -0300

    finalizando o exercício

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import { Button } from "../components/Button";
+import { Container } from "../components/Container";
+import { ThemeSwitch } from "../components/ThemeSwitch";
+import { ThemeProvider } from "../contexts/ThemeContext";
+
 const Page = () =>{
   return (
-    <div>...</div>
+    <ThemeProvider>
+      <Container>
+        <header className="py-5">
+          <h1 className="text-3xl">Título da página</h1>
+        </header>
+        <section>
+          <p className="my-5">Conteúdo da página</p>
+          <Button label="Clique aqui" onClick={()=> {}} />
+        </section>
+        <ThemeSwitch />
+      </Container>
+    </ThemeProvider>
   )
 }
 
